63ee14774c591f309dca61c9cad39ab6b05f174d
Rafael Andrade <rafaelrki123@gmail.com>
2023-04-13 11:44:16 -0300

    Terminei o jogo 1

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
+function apareceTexto (idElementoTexto) {
+    document.querySelector(idElementoTexto).style.opacity = '100%';
+}
+
 const listaAnimais = document.querySelectorAll('.animal');
 
 for (let contador = 0; contador < listaAnimais.length; contador++) {
@@ -9,9 +13,13 @@ for (let contador = 0; contador < listaAnimais.length; contador++) {
     const animal = listaAnimais[contador];
     const nomeAnimal = animal.classList[1];
     const idAudio = `#som_${nomeAnimal}`;
+    const idTexto = `#txt_${nomeAnimal}`;
 
     animal.onclick = function() {
         tocaSom(idAudio);
+        apareceTexto(idTexto);
     }
 }
+
+
 
